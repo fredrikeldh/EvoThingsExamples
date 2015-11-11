080a4f202cda172aeaecd97c57dec5f23484d26d
Fredrik Eldh <fredrik.eldh@evothings.com>
2015-11-11 17:06:02 +0100

    mqtt-painter: added actual mqtt support.

--- experiments/mqtt-painter/app.js
+++ experiments/mqtt-painter/app.js
@@ -4,6 +4,10 @@
  */
 var app = {};
 
+app.connected = false;
+app.sending = false;
+app.recieveing = true;
+
 app.initialize = function() {
 	document.addEventListener(
 		'deviceready',
@@ -12,8 +16,13 @@ app.initialize = function() {
 }
 
 app.onReady = function() {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext('2d');
+	app.setupCanvas();
+	app.setupConnection();
+}
+
+app.setupCanvas = function() {
+	app.canvas = document.getElementById("canvas");
+	app.ctx = app.canvas.getContext('2d');
 	var left, top;
 	{
 		var totalOffsetX = 0;
@@ -24,17 +33,93 @@ app.onReady = function() {
 			totalOffsetX += curElement.offsetLeft;
 			totalOffsetY += curElement.offsetTop;
 		} while(curElement = curElement.offsetParent)
-		left = totalOffsetX;
-		top = totalOffsetY;
+		app.left = totalOffsetX;
+		app.top = totalOffsetY;
 	}
-	console.log(left+" "+top);
-	ctx.beginPath();
+	console.log(app.left+"x"+app.top);
+	app.ctx.beginPath();
 	canvas.addEventListener("touchmove", function(event) {
 		var t = event.touches[0];
-		//console.log(Math.floor(t.clientX)+"x"+Math.floor(t.clientY));
-		ctx.lineTo(Math.floor(t.clientX) - left, Math.floor(t.clientY) - top);
-		ctx.stroke();
+		var x = Math.floor(t.clientX) - app.left;
+		var y = Math.floor(t.clientY) - app.top;
+		app.ctx.lineTo(x, y);
+		app.ctx.stroke();
+		if(app.connected && app.sending) {
+			var msg = JSON.stringify({x:x,y:y})
+			app.publish(msg);
+			//console.log(msg);
+		}
 	});
+}
+
+app.setupConnection = function() {
+	var hostname = 'backup.evothings.com';
+	//var hostname = '192.168.1.100';
+	var port = 1883;
+	var clientId = 'painter_'+Math.floor(Math.random()*100);
+	app.mqttClient = new Paho.MQTT.Client(hostname, port, clientId);
+  app.status("Connecting...");
+	//app.mqttClient.onConnectionLost = app.onConnectionLost;	// todo
+	app.mqttClient.onMessageArrived = app.onMessageArrived;
+	app.mqttClient.connect({onSuccess:app.onConnect,
+		invocationContext: {host : hostname, port: port, clientId: clientId}
+	});
+}
+
+app.publish = function(message) {
+	var topic = '/test';
+	var qos = 1;
+	//console.log('Publishing Message: Topic: \''+topic+'\'. QoS: ' + qos + '. Message: '+message);
+	message = new Paho.MQTT.Message(message);
+	message.destinationName = topic;
+	message.qos = qos;
+	app.mqttClient.send(message);
+};
+
+app.subscribe = function() {
+	app.mqttClient.subscribe('/test');
+}
+
+app.unsubscribe = function() {
+	app.mqttClient.unsubscribe('/test');
+}
+
+app.onMessageArrived = function(message) {
+  //console.log("onMessageArrived:"+message.payloadString);
+	var o = JSON.parse(message.payloadString);
+	app.ctx.lineTo(o.x, o.y);
+	app.ctx.stroke();
+}
+
+app.onConnect = function(context) {
+	app.status("Connected!");
+	app.connected = true;
+	if(app.recieveing)
+		app.subscribe();
+}
+
+app.onSend = function() {
+	if(app.connected) {
+		app.status("Sending...");
+		app.sending = true;
+		app.recieveing = false;
+		app.unsubscribe();
+	}
+}
+
+app.onRecieve = function() {
+	if(app.connected) {
+		app.status("Recieveing...");
+		app.recieveing = true;
+		app.sending = false;
+		app.subscribe();
+	}
+}
+
+app.status = function(s) {
+	console.log(s);
+	var info = document.getElementById("info");
+	info.innerHTML = s;
 }
 
 app.initialize();
